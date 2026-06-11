d23fda64e08a18a021552280c8b585c51a8a90dd
BakasuraRCE <bakasura@protonmail.ch>
2026-06-10 22:35:49 -0500

    chore(tools): Add GPL license header to NZB obfuscation check tool
    - Add GNU General Public License v3 header with copyright attribution
    - Include author contact and project repository reference
    - Align licensing with project standards and requirements

--- tools/nzb_obfuscation_check.py
+++ tools/nzb_obfuscation_check.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
+#
+# Copyright (C) 2026 BakasuraRCE <bakasura@protonmail.ch>
+# This file is a part of ngPostEx : https://github.com/BakasuraRCE/ngPostEx
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>
+#
 """
 nzb_obfuscation_check.py - Analyze obfuscation level of an NZB file.
 
